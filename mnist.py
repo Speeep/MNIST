1f8712370b253223e532a3db818fd60c0c695048
Giovanni Giacalone <65633514+Speeep@users.noreply.github.com>
2021-02-23 20:01:51 -0500

    Update mnist.py

--- mnist.py
+++ mnist.py
@@ -9,9 +9,9 @@ import numpy as np
 def create_model():
     model = tf.keras.models.Sequential()
     model.add(tf.keras.layers.Flatten())
-    model.add(tf.keras.layers.Dense(32, activation=tf.nn.relu))
-    model.add(tf.keras.layers.Dense(32, activation=tf.nn.relu))
-    model.add(tf.keras.layers.Dense(32, activation=tf.nn.relu))
+    model.add(tf.keras.layers.Dense(128, activation=tf.nn.relu))
+    model.add(tf.keras.layers.Dense(128, activation=tf.nn.relu))
+    model.add(tf.keras.layers.Dense(128, activation=tf.nn.relu))
     model.add(tf.keras.layers.Dense(10, activation=tf.nn.softmax))
 
     model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
